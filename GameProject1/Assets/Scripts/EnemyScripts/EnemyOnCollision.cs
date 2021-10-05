7a76cf1b8cc50c8428b7c068a2bab336755f98a8
nuflol <Albin.Forsberg@futuregames.nu>
2021-10-05 14:18:35 +0200

    Fixed the collision between enemy and play projectiles

--- GameProject1/Assets/Scripts/EnemyScripts/EnemyOnCollision.cs
+++ GameProject1/Assets/Scripts/EnemyScripts/EnemyOnCollision.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyOnCollision : MonoBehaviour {
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnTriggerEnter2D(Collider2D collision) {
             if (collision.gameObject.CompareTag("Projectile")) {
                 Debug.Log("Hit");
             }
-        }
+    }
 }
