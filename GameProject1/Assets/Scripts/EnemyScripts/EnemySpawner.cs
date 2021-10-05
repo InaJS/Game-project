649403c057f05f2c2e22eb76a27e8c12b019ac44
nuflol <Albin.Forsberg@futuregames.nu>
2021-10-05 18:21:47 +0200

    +Added enemy spawn zones

--- GameProject1/Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ GameProject1/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -2,22 +2,31 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class EnemySpawner : MonoBehaviour {
     [SerializeField] private List <EnemyOnCollision> enemyList = new List <EnemyOnCollision>();
+    [SerializeField] private bool enableGizmo;
+    [Range(0f, 25f)] [SerializeField] private float cubeHeight = 5f;
+    [Range(0f, 25f)] [SerializeField] private float cubeWidth = 5f;
     private float spawnTime = 3f;
     private Vector3 spawnPosition;
-    private bool enableGizmo;
+    private Vector3 sizeOfCube;
 
     private void Start() {
+        sizeOfCube = new Vector3(cubeWidth, cubeHeight);
         InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
     void Spawn() {
-        spawnPosition.x = Random.Range(-17, 17);
-        spawnPosition.y = Random.Range(-17, 17);
+        spawnPosition.x = Random.Range(transform.position.x - cubeWidth / 2 , transform.position.x + cubeWidth / 2);
+        spawnPosition.y = Random.Range(transform.position.y - cubeHeight /2 , transform.position.y + cubeHeight / 2);
         Instantiate(enemyList[Random.Range(0, enemyList.Count)], spawnPosition, Quaternion.identity);
+    }
+
+    private void OnValidate() {
+        sizeOfCube = new Vector3(cubeWidth, cubeHeight);
     }
 
     private void OnDrawGizmos() {
@@ -26,5 +35,6 @@ public class EnemySpawner : MonoBehaviour {
             return;
         }
         
+        Gizmos.DrawWireCube(transform.position, sizeOfCube);
     }
 }
