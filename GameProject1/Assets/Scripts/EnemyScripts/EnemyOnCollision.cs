c7304c4597e2096569cc4e66c82447fc4ee144a7
nuflol <Albin.Forsberg@futuregames.nu>
2021-10-05 13:25:34 +0200

    Made changes to the enemy AI

--- GameProject1/Assets/Scripts/EnemyScripts/EnemyOnCollision.cs
+++ GameProject1/Assets/Scripts/EnemyScripts/EnemyOnCollision.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyOnCollision : MonoBehaviour {
     private void OnCollisionEnter2D(Collision2D collision) {
-            if (collision.gameObject.tag == "Projectile") {
+            if (collision.gameObject.CompareTag("Projectile")) {
                 Debug.Log("Hit");
             }
         }
