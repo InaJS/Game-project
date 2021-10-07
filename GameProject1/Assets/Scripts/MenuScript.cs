3b08bd5450c4553fe7d2699154ab0c7797387589
InaJS <esalmenranta@gmail.com>
2021-10-07 17:55:32 +0200

    Menu script

---
+++ GameProject1/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//To load a new scene, UnityEngine.SceneManagement needs to be in use.
+public class Menu : MonoBehaviour
+{
+
+    [SerializeField] GameObject pauseMenu;
+
+    public static bool GameIsPaused = false;
+
+    public void Play()
+    {
+        SceneManager.LoadScene("LevelArena");
+
+    }
+
+    
+    //Quit the Application.
+    public void Quit()
+    {
+        //Won't happen in the Editor. So to make sure it's working.
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Rules ()
+    {
+        SceneManager.LoadScene("Rules");
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+}
