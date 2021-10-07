f300f06e8e6281197f59d39bf3a4c23c44f34412
5Daydreams <nelsonkossuga@gmail.com>
2021-10-07 11:10:22 +0200

    + Adjusted the tiling issue with the prefabs deleting game objects;

---
+++ GameProject1/Assets/Scripts/SceneLights/DanceFloorLightingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DanceFloorLightingController : MonoBehaviour
+{
+    [SerializeField] private FloorOrganizer organizer;
+    [Range(1,10)] [SerializeField] private int lightGroupCount;
+    [SerializeField] private List<LightGroup> tileLights;
+
+    private void Start()
+    {
+        tileLights.Clear();
+        tileLights.Capacity = lightGroupCount;
+
+        List<SpriteRenderer> allTiles = organizer.Tiles;
+
+        int aproxSplit = allTiles.Count/lightGroupCount;
+
+        for (int i = 0; i < lightGroupCount; i++)
+        {
+            while (tileLights[i].renderers.Count < aproxSplit)
+            {
+                SpriteRenderer randomTile = allTiles[Random.Range(0, allTiles.Count)];
+                tileLights[i].renderers.Add(randomTile);
+                allTiles.Remove(randomTile);
+            }
+        }
+    }
+
+    // private void OnValidate() TODO
+    // {
+    //     tileLights.Capacity = lightGroupCount;
+    // }
+}
+
+[Serializable]
+public class LightGroup
+{
+    public List<SpriteRenderer> renderers;
+}
--- GameProject1/Assets/Scripts/Tools/FloorOrganizer.cs
+++ GameProject1/Assets/Scripts/Tools/FloorOrganizer.cs
@@ -1,37 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FloorOrganizer : MonoBehaviour
 {
-    [SerializeField] private GameObject tilePrefab;
+    [SerializeField] private SpriteRenderer tilePrefab;
+    [SerializeField] private GameObject tilesHolder;
     [SerializeField] private float tileScale;
+    private List<SpriteRenderer> tiles = new List<SpriteRenderer>();
 
+    public List<SpriteRenderer> Tiles => tiles;
 
-    [Header("Horizontal")]
-    [SerializeField] private int horizontalCount;
+    [Header("Horizontal")] [SerializeField]
+    private int horizontalCount;
+
     [SerializeField] private float horizontalSpacing;
 
-    [Header("Vertical")]
-    [SerializeField] private int vertitalCount;
+    [Header("Vertical")] [SerializeField] private int vertitalCount;
     [SerializeField] private float vertitalSpacing;
-
 
     public void Reorganize()
     {
-        for (int i = this.transform.childCount - 1; i >= 0; i--)
+        for (int i = tilesHolder.transform.childCount - 1; i >= 0; i--)
         {
-            DestroyImmediate(transform.GetChild(i).gameObject);
+            DestroyImmediate(tilesHolder.transform.GetChild(i).gameObject);
         }
 
         for (int i = 0; i < horizontalCount; i++)
         {
             for (int j = 0; j < vertitalCount; j++)
             {
-                Vector3 tilePos = transform.position + new Vector3(i*horizontalSpacing, j*vertitalSpacing,0);
-                
-                GameObject newTile = Instantiate(tilePrefab,tilePos,Quaternion.identity,this.transform);
-                newTile.transform.localScale = Vector3.one * tileScale;
+                Vector3 tilePos = tilesHolder.transform.position + new Vector3(i * horizontalSpacing, j * vertitalSpacing, 0);
+
+                SpriteRenderer newTile = Instantiate(tilePrefab, tilePos, Quaternion.identity, tilesHolder.transform);
+                newTile.gameObject.transform.localScale = Vector3.one * tileScale;
+                newTile.gameObject.name = "(" + i + "," + j + ")";
+                tiles.Add(newTile);
             }
         }
     }
