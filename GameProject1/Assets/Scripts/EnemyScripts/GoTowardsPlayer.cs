acf2dd806d41642d48859c44cdb97569dacdc22f
5Daydreams <nelsonkossuga@gmail.com>
2021-10-07 13:43:09 +0200

    More prefab adjustments;

--- GameProject1/Assets/Scripts/EnemyScripts/GoTowardsPlayer.cs
+++ GameProject1/Assets/Scripts/EnemyScripts/GoTowardsPlayer.cs
@@ -3,24 +3,29 @@ using UnityEngine;
 using System.Collections.Generic;
 
 
-public class GoTowardsPlayer : MonoBehaviour {
-        [SerializeField] private float moveSpeed = 2f;
-        [SerializeField] private float minDistance = 0.5f;
-        [SerializeField] private Animator myAnim;
-        public Transform target;
+public class GoTowardsPlayer : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float minDistance = 0.5f;
+    [SerializeField] private Animator myAnim;
+    public Transform target;
 
-        private void Awake() {
-              target = GameObject.FindGameObjectWithTag("Player").transform;
-              myAnim = GetComponentInChildren<Animator>();
+    private void Awake()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        myAnim = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if ((target.position - transform.position).magnitude < minDistance)
+        {
+            return;
         }
 
-        void Update() {
-                if ((target.position - transform.position).magnitude < minDistance) {
-                        return;
-                } 
-                float stepDistance = Time.deltaTime * moveSpeed;
-                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, stepDistance);
-                myAnim.SetFloat("moveX", (target.position - transform.position).normalized.x);
-                myAnim.SetFloat("moveY",(target.position - transform.position).normalized.y);
-        }
+        float stepDistance = Time.deltaTime * moveSpeed;
+        transform.position = Vector3.MoveTowards(transform.position, target.transform.position, stepDistance);
+        myAnim.SetFloat("moveX", (target.position - transform.position).normalized.x);
+        myAnim.SetFloat("moveY", (target.position - transform.position).normalized.y);
+    }
 }
--- GameProject1/Assets/Scripts/PlayerScripts/PlayerShooting.cs
+++ GameProject1/Assets/Scripts/PlayerScripts/PlayerShooting.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShooting : MonoBehaviour 
+public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] private float bulletSpeed;
     [SerializeField] private float fireRate = 10f;
@@ -12,34 +12,40 @@ public class PlayerShooting : MonoBehaviour
 
     private float shootCooldown;
     private Vector3 target;
-    void Start() {
-        float shootCooldown = 1.0f / fireRate;
+
+    void Start()
+    {
+        shootCooldown = 1.0f / fireRate;
         Cursor.visible = false;
     }
-    
-    void Update() {
+
+    void Update()
+    {
         shootCooldown -= Time.deltaTime;
-        target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,Input.mousePosition.y, transform.position.z));
+        target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
         crosshairs.transform.position = new Vector2(target.x, target.y);
 
         Vector3 difference = target - this.transform.position;
         float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
 
-        if (Input.GetMouseButton(0)) {
-            if (shootCooldown < 0) {
+        if (Input.GetMouseButton(0))
+        {
+            if (shootCooldown < 0)
+            {
                 float distance = difference.magnitude;
                 Vector2 direction = difference / distance;
                 direction.Normalize();
-                fireBullet(direction, rotationZ);   
+                fireBullet(direction, rotationZ);
                 shootCooldown = 1.0f / fireRate;
             }
         }
     }
 
-    void fireBullet(Vector2 direction, float rotationZ) {
+    void fireBullet(Vector2 direction, float rotationZ)
+    {
         GameObject b = Instantiate(bulletPrefab) as GameObject;
         b.transform.position = bulletStart.transform.position;
         b.transform.rotation = Quaternion.Euler(0.0f, 0.0f, rotationZ);
-        b.GetComponent <Rigidbody2D>().velocity = direction * bulletSpeed;
+        b.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
     }
 }
