f35ae47807ec16f16393461a0614d9a8312a8ddf
5Daydreams <nelsonkossuga@gmail.com>
2021-10-08 18:41:14 +0200

    Enemy spawning is broken; I'm still working on the wave system;

--- GameProject1/Assets/Scripts/EnemySpawning/EnemySpawner.cs
+++ GameProject1/Assets/Scripts/EnemySpawning/EnemySpawner.cs
@@ -5,41 +5,57 @@ using UnityEngine;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
-public class EnemySpawner : MonoBehaviour {
+public class EnemySpawner : MonoBehaviour
+{
     [SerializeField] private List<EnemyWave> enemyWaveList;
     [SerializeField] private bool enableGizmo;
+    [SerializeField] private List<SpawnZone> spawnZones;
+    
     [Range(0f, 25f)] [SerializeField] private float cubeHeight = 5f;
     [Range(0f, 25f)] [SerializeField] private float cubeWidth = 5f;
     [SerializeField] private float spawnTime = 3f;
-    private Vector3 spawnPosition;
-    private Vector3 sizeOfCube;
 
-    private void Start() {
-        sizeOfCube = new Vector3(cubeWidth, cubeHeight);
+    private void Start()
+    {
         InvokeRepeating("Spawn", spawnTime, spawnTime);
     }
 
-    void Spawn() {
-        spawnPosition.x = Random.Range(transform.position.x - cubeWidth / 2 , transform.position.x + cubeWidth / 2);
-        spawnPosition.y = Random.Range(transform.position.y - cubeHeight /2 , transform.position.y + cubeHeight / 2);
+    public void SpawnOnZone(SpawnZone zone)
+    {
+        EnemyWave randomWave = enemyWaveList[Random.Range(0, enemyWaveList.Count)];
+        EnemyAI randomEnemy = randomWave.enemies[Random.Range(0, enemyWaveList.Count)].Enemy;
 
-        Instantiate(enemyWaveList[Random.Range(0, enemyWaveList.Count)], spawnPosition, Quaternion.identity);
+        Spawn(randomEnemy, zone);
     }
 
-    private void OnValidate() {
-        sizeOfCube = new Vector3(cubeWidth, cubeHeight);
+    void Spawn(EnemyAI enemy, SpawnZone zone)
+    {
+        Vector3 spawnPosition = new Vector3();
+        spawnPosition.x = Random.Range(transform.position.x - cubeWidth / 2, transform.position.x + cubeWidth / 2);
+        spawnPosition.y = Random.Range(transform.position.y - cubeHeight / 2, transform.position.y + cubeHeight / 2);
 
-        // Some really cursed transform.scale stuff happened, I just did a minor fix to adjust it
-        // old code for reference
-        // sizeOfCube = new Vector3(cubeWidth, cubeHeight);
+        Instantiate(enemy, spawnPosition, Quaternion.identity);
     }
-
-    private void OnDrawGizmos() {
+    
+    private void OnDrawGizmos()
+    {
         if (!enableGizmo)
         {
             return;
         }
-        
-        Gizmos.DrawWireCube(transform.position, sizeOfCube);
+
+        foreach (var zone in spawnZones)
+        {
+            var sizeOfCube = new Vector3(zone.Width, zone.Height);
+            Gizmos.DrawWireCube(zone.Center, sizeOfCube);
+        }
     }
+}
+
+[Serializable]
+public class SpawnZone
+{
+    public Vector3 Center;
+    [Range(0f, 25f)] public float Width;
+    [Range(0f, 25f)] public float Height;
 }
