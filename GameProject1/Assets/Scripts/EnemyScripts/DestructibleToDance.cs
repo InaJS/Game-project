de2427efc2b2e48a26a6b0435bb36f8f69bbedfa
5Daydreams <nelsonkossuga@gmail.com>
2021-10-12 16:25:18 +0200

    Adjusting minor console errors;

---
+++ GameProject1/Assets/Scripts/EnemyScripts/DestructibleToDance.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+
+public class DestructibleToDance : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Dance"))
+        {
+            return;
+        }
+
+        BreakObject();
+    }
+
+    private void BreakObject()
+    {
+        Destroy(this.gameObject);
+    }
+}
