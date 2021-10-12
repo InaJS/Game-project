ffbd73d7227aedebc220ac05499549c652ae5efa
5Daydreams <nelsonkossuga@gmail.com>
2021-10-12 12:29:37 +0200

    + Adding VFX for player damage;

--- GameProject1/Assets/Scripts/PlayerScripts/DamageFeedback.cs
+++ GameProject1/Assets/Scripts/PlayerScripts/DamageFeedback.cs
@@ -1,7 +1,14 @@
-﻿namespace PlayerScripts
+﻿using UnityEngine;
+
+public class DamageFeedback : MonoBehaviour
 {
-    public class DamageFeedback
+    [SerializeField] private GameObject VFXPrefab;
+    [SerializeField] private float particlesLifetime = 1.5f;
+
+    public void SpawnVFX()
     {
+        GameObject particles = Instantiate(VFXPrefab, this.transform.position, Quaternion.identity, null);
         
+        Destroy(particles, particlesLifetime);
     }
 }
